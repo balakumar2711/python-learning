fef19d782d4c0a0732424f4feb9735d53d91cd5a
Balakumar <sbalakumar001@gmail.com>
2025-11-06 10:34:41 +0530

    Added the `Complete` functionality to the web app 06 Nov

--- web.py
+++ web.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from streamlit import session_state
+
 import functions
 
 todos = functions.read_fun() #Reads the text file present
@@ -13,8 +15,15 @@ st.title("My App") #The name of the app
 st.subheader("The tasks to be done") #Sub headers if any
 st.write("This is to capture my works") #The next level of sub header
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    #Here the value of checkbox when printed by session state will be true or false based on the selection so a dynamic variable is added for the key which makes it easy to capture the checkbox and pop it out using the index
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_fun(todos)
+        #The below session state updates are required for the program to update the web page in real time
+        del st.session_state[todo]
+        st.rerun()
 
 st.text_input(label="", placeholder="Enter the new item",
               on_change=add_todo, key='new_todo')
